84f8cdeb95adc4d4c0cdab8110e1c606a5a9cb88
Akash Gupta <akash1128355@gmail.com>
2023-12-26 17:29:35 +0530

    tsconfig file updated errors resolved

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -6,7 +6,8 @@ import {
 } from "./types";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:8080` }),
+  // baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:8080` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `https://finance-app-new-chi.vercel.app/` }),
   reducerPath: "main",
   tagTypes: ["Kpis", "Products", "Transactions"],
   endpoints: (build) => ({
